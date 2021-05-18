ca30c631ebd0939ee1cc2e33311162dfcbc03fdb
Jeff Yang <jho.yang96@gmail.com>
2021-05-17 23:52:58 -0500

    added border radius of 5 for image tiles

--- src/components/ImageTile/styles.js
+++ src/components/ImageTile/styles.js
@@ -1,14 +1,15 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-    image: {
-        height: 250,
-        width: 250,
-        marginLeft: 25,
-        marginRight: 25,
-        marginTop: 35,
-        marginBottom: 10
-    }
+  image: {
+    height: 250,
+    width: 250,
+    marginLeft: 25,
+    marginRight: 25,
+    marginTop: 35,
+    marginBottom: 10,
+    borderRadius: 5
+  }
 });
 
 export default styles;
