722778680524041246064dbb5c8d30194aaa0b6b
Jeff Yang <jho.yang96@gmail.com>
2021-05-17 23:24:39 -0500

    social gallery created-by text align center

--- src/components/Gallery/styles.js
+++ src/components/Gallery/styles.js
@@ -1,31 +1,31 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-    layout: {
-        flex: 1
-    },
-    imageWrapper: {
-        flex: 1,
-        //marginTop: '5%',
-        marginBottom: '5%',
-        marginLeft: '15%',
-        marginRight: '15%',
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        justifyContent: 'center',
-    },
-    scrollView: {
-        backgroundColor: 'white',
-        flex: 1,
-        height: "100%",
-    },
-    createdByText: {
-        marginLeft: 25,
-        marginBottom: 10,
-        color: "#7d7d7d",
-        fontSize: 14,
-        fontWeight: "300"
-    }
+  layout: {
+    flex: 1
+  },
+  imageWrapper: {
+    flex: 1,
+    //marginTop: '5%',
+    marginBottom: '5%',
+    marginLeft: '15%',
+    marginRight: '15%',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center'
+  },
+  scrollView: {
+    backgroundColor: 'white',
+    flex: 1,
+    height: '100%'
+  },
+  createdByText: {
+    marginBottom: 10,
+    color: '#7d7d7d',
+    fontSize: 14,
+    fontWeight: '300',
+    alignSelf: 'center'
+  }
 });
 
 export default styles;
