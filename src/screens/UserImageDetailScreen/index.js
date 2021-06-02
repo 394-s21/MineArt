2da40bb8fae628fe8f07093bf9d210b0be9ce7af
Peizhi Liu <peizhiliu2023@u.northwestern.edu>
2021-06-02 11:04:23 -0400

    using full image in UserImageDetailScreen

--- src/screens/UserImageDetailScreen/index.js
+++ src/screens/UserImageDetailScreen/index.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect }  from 'react';
-import { SafeAreaView } from 'react-native';
+import React, { useRef, useState, useEffect }  from 'react';
+import { SafeAreaView, Animated } from 'react-native';
 import { Text, Layout } from '@ui-kitten/components';
 import styles from './styles';
 import ImageTile from '../../components/ImageTile';
@@ -8,35 +8,54 @@ import { useFirebaseContext } from '../../providers/firebaseProvider';
 
 const UserImageDetailScreen = ({ route, navigation }) => {
   const { id, user, imageUrl } = route.params;
-  const [descrip, setDescrip] = useState("");
   const firebase = useFirebaseContext();
+  const storage = firebase.storage();
   const db = firebase.firestore();
+
+  const [descrip, setDescrip] = useState("");
+  const [pieceURL, setPieceURL] = useState('');
+  const opacity = useRef(new Animated.Value(0)).current;
+
+  const onLoad = () => {
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: 500,
+      useNativeDriver: true
+    }).start();
+  };
+
   useEffect(() => {
     if (!id) return;
     db.collection("social-feed")
       .doc(id)
       .get()
-      .then(doc => {
+      .then(async (doc) => {
         setDescrip(doc.get("description"))
+        let url = await storage.ref(doc.get("image")).getDownloadURL();
+        setPieceURL(url)
       })
   }, [id])
 
-  console.log(id);
   return (
     <SafeAreaView style={styles.safeAreaView}>
       <Layout style={styles.layout}>
-        <ImageTile
-          imageUrl={imageUrl}
-          disableOnPress={true}
-          imgWidth={330}
-          imgHeight={330}
+        <Animated.Image
+          onLoad={onLoad}
+          source={{ uri: pieceURL }}
+          resizeMode="contain"
+          style={[
+            {
+              height: "80%",
+              width: "90%",
+              opacity: opacity,
+              marginTop: 10
+            }
+          ]}
         />
         <Text style={styles.textBoxTitle} category="h6">
           A peek into the creator's mind...
         </Text>
-        <TextBox>
-          {descrip}
-        </TextBox>
+        {descrip ? <TextBox>{descrip}</TextBox> : null}
       </Layout>
     </SafeAreaView>
   );
