ee8ee8034d29478f335007c740520a018b2453ca
Aura Ulloa-Ordonez <auraulloa.9000@gmail.com>
2021-06-08 15:32:06 -0500

    init branch

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,13 @@ module.exports = {
       '|@react-navigation' +
       '|@react-native-community' +
       '|react-native-iphone-x-helper' +
-      '/)'
-  ]
+      '/)',
+      //"node_modules/(?!expo-file-system/)"
+  ],
+  coveragePathIgnorePatterns:[
+    "node_modules/expo-file-system"
+  ],
+  "moduleNameMapper": {
+    "\\.(css|scss)$": "<rootDir>/__mocks__/styleMock.js"
+  }
 };
